ecc3987991b1ce986bc6e47cd1e9074d3edc9a73
emersonzzz <er7682ncr1@stu.nc.k12.mo.us>
2025-04-03 10:57:56 -0500

    worked on dynamic listings

--- static/js/script.js
+++ static/js/script.js
@@ -85,58 +85,57 @@ async function fetchListings(tags = []) {
     const listings = [{
         id: 1,
         name: "House1",
-        tags: ["garden", "pool", "wifi"]
-    }] // await response.json()
+        tags: ["garden", "pool", "wifi"],
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-TfDAisMrcoxUe9MYTXeLYkiCT8uLSkMYog&s"
+    },
+    {
+        id: 2,
+        name: "House2",
+        tags: ["garden"],
+        icon: "https://imgcdn.stablediffusionweb.com/2024/9/27/4b3b11af-fd3a-4a8d-8191-1392be3c6f9a.jpg"
+    }
+    ] // await response.json()
 
     const container = document.getElementById('options')
     container.innerHTML = ''
 
     listings.forEach(listing => {
         const parent = document.createElement('div')
-        
         const option = document.createElement('div')
         option.classList.add('option')
-
         const splash = document.createElement('div')
         splash.classList.add('splash')
-
         const background = document.createElement('div')
         background.classList.add('background')
-        background.style.background = `METHOD TO GET URL HERE`
-
+        background.style.background = `url('${listing.icon}') center center / cover no-repeat`
         splash.appendChild(background)
-
         const icon = document.createElement('img')
         icon.classList.add('icon')
-        icon.src = `METHOD TO GET URL HERE`
-
+        icon.src = listing.icon
         splash.appendChild(icon)
         option.appendChild(splash)
-
         const info = document.createElement('div')
         info.classList.add('info')
-
         const details = document.createElement('div')
         details.classList.add('details')
-
         const name = document.createElement('h3')
         name.classList.add('name')
         name.textContent = listing.name
         details.appendChild(name)
-
         const city = document.createElement('div')
         city.classList.add('city')
         city.textContent = 'TEST'
-        details.appendChild('city')
+        details.appendChild(city)
         info.appendChild(details)
-
         const open = document.createElement('button')
         open.classList.add('open')
-
+        open.onclick = () => {
+            window.location.href = `/listings/${listing.id}`
+        }
         const apply = document.createElement('div')
         apply.classList.add('apply')
-
         const btntext = document.createElement('span')
+        btntext.href = `/listing/${listing.id}`
         btntext.textContent = 'Apply'
         apply.appendChild(btntext)
         open.appendChild(apply)
