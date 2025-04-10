f3503b3bc7db2d32f46c4edda6ddeeaa76703f9b
emersonzzz <er7682ncr1@stu.nc.k12.mo.us>
2025-04-09 21:24:39 -0500

    Worked on approve listings

--- static/js/script.js
+++ static/js/script.js
@@ -30,79 +30,75 @@ window.onclick = function(event) {
     }
 }
 
-const contactopen = document.getElementById('contact-open')
-const contactmodal = document.getElementById('contactModal')
+document.addEventListener('DOMContentLoaded', () => {
 
-contactopen.addEventListener('click', () => {
-    contactmodal.classList.remove('hidden')
-})
+    const contactopen = document.getElementById('contact-open')
+    const contactmodal = document.getElementById('contactModal')
 
-const optionquestion = document.getElementById('question')
-const optionmeeting = document.getElementById('arrangeMeeting')
-const optionmental = document.getElementById('mentalHealth')
-const optionrelationship = document.getElementById('relationshipCounseling')
-const optionbusiness = document.getElementById('businessInquiry')
-const optiontip = document.getElementById('tipLine')
+    contactopen.addEventListener('click', () => {
+        contactmodal.classList.remove('hidden')
+    })
 
-optionquestion.addEventListener('click', () => {
-    contactmodal.classList.add('hidden')
-    contactquestion.classList.remove('hidden')
-})
+    const optionquestion = document.getElementById('question')
+    const optionmeeting = document.getElementById('arrangeMeeting')
+    const optionmental = document.getElementById('mentalHealth')
+    const optionrelationship = document.getElementById('relationshipCounseling')
+    const optionbusiness = document.getElementById('businessInquiry')
+    const optiontip = document.getElementById('tipLine')
 
-optionmeeting.addEventListener('click', () => {
-    contactmodal.classList.add('hidden')
-    contactmeeting.classList.remove('hidden')
-})
+    optionquestion.addEventListener('click', () => {
+        contactmodal.classList.add('hidden')
+        contactquestion.classList.remove('hidden')
+    })
 
-optionmental.addEventListener('click', () => {
-    contactmodal.classList.add('hidden')
-    contactmental.classList.remove('hidden')
-})
+    optionmeeting.addEventListener('click', () => {
+        contactmodal.classList.add('hidden')
+        contactmeeting.classList.remove('hidden')
+    })
 
-optionrelationship.addEventListener('click', () => {
-    contactmodal.classList.add('hidden')
-    contactrelationship.classList.remove('hidden')
-})
+    optionmental.addEventListener('click', () => {
+        contactmodal.classList.add('hidden')
+        contactmental.classList.remove('hidden')
+    })
 
-optionbusiness.addEventListener('click', () => {
-    contactmodal.classList.add('hidden')
-    contactbusiness.classList.remove('hidden')
-})
+    optionrelationship.addEventListener('click', () => {
+        contactmodal.classList.add('hidden')
+        contactrelationship.classList.remove('hidden')
+    })
 
-optiontip.addEventListener('click', () => {
-    contactmodal.classList.add('hidden')
-    contacttip.classList.remove('hidden')
-})
+    optionbusiness.addEventListener('click', () => {
+        contactmodal.classList.add('hidden')
+        contactbusiness.classList.remove('hidden')
+    })
 
-const postopen = document.getElementById('post')
+    optiontip.addEventListener('click', () => {
+        contactmodal.classList.add('hidden')
+        contacttip.classList.remove('hidden')
+    })
 
-postopen.addEventListener('click', () => {
-    postlisting.classList.remove('hidden')
-})
+    const postopen = document.getElementById('post')
 
-const anncpost = document.getElementById('postannouncements')
-const anncmodal = document.getElementById('postAnnouncementModal')
+    postopen.addEventListener('click', () => {
+        postlisting.classList.remove('hidden')
+    })
 
-anncpost.addEventListener('click', () => {
-    anncmodal.classList.remove('hidden')
-})
+    const cookieclose = document.getElementById('cookies-close')
+    const cookiebanner = document.getElementById('cookies')
 
-const cookieclose = document.getElementById('cookies-close')
-const cookiebanner = document.getElementById('cookies')
+    cookieclose.addEventListener('click', () => {
+        cookiebanner.remove()
+    })
 
-cookieclose.addEventListener('click', () => {
-    cookiebanner.remove()
-})
+    const navregister = document.getElementById('navregister')
+    const navlogin = document.getElementById('navlogin')
 
-const navregister = document.getElementById('navregister')
-const navlogin = document.getElementById('navlogin')
+    navregister.addEventListener('click', () => {
+        window.location.href = '/register'
+    })
 
-navregister.addEventListener('click', () => {
-    window.location.href = '/register'
-})
-
-navlogin.addEventListener('click', () => {
-    window.location.href = '/login'
+    navlogin.addEventListener('click', () => {
+        window.location.href = '/login'
+    })
 })
 
 // Extracted from listings.js //
@@ -110,6 +106,7 @@ navlogin.addEventListener('click', () => {
 const postform = document.getElementById('postModalForm')
 
 postform.addEventListener('submit', async (e) => {
+    console.log(`POSTED`)
     e.preventDefault()
     const name = document.getElementById('postname').value
     const icon = document.getElementById('posticon').value
@@ -200,6 +197,72 @@ async function fetchListings(tags = []) {
         const btntext = document.createElement('span')
         btntext.href = `/listing/${listing.id}`
         btntext.textContent = 'Apply'
+        apply.appendChild(btntext)
+        open.appendChild(apply)
+        info.appendChild(open)
+        option.appendChild(info)
+        parent.appendChild(option)
+        container.appendChild(parent)
+    })
+}
+
+async function fetchListingsForReview() {
+    const response = await fetch('/searchraw', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({  })
+    })
+    
+    const data = await response.json()
+    console.log(data)
+
+    const listings = data.listings
+    
+    if (listings.length == 0) {
+        return
+    }
+
+    const container = document.getElementById('options')
+    container.innerHTML = ''
+
+    listings.forEach(listing => {
+        const parent = document.createElement('div')
+        const option = document.createElement('div')
+        option.classList.add('option')
+        const splash = document.createElement('div')
+        splash.classList.add('splash')
+        const background = document.createElement('div')
+        background.classList.add('background')
+        background.style.background = `url('${listing.icon}') center center / cover no-repeat`
+        splash.appendChild(background)
+        const icon = document.createElement('img')
+        icon.classList.add('icon')
+        icon.src = listing.icon
+        splash.appendChild(icon)
+        option.appendChild(splash)
+        const info = document.createElement('div')
+        info.classList.add('info')
+        const details = document.createElement('div')
+        details.classList.add('details')
+        const name = document.createElement('h3')
+        name.classList.add('name')
+        name.textContent = listing.name
+        details.appendChild(name)
+        const city = document.createElement('div')
+        city.classList.add('city')
+        city.textContent = listing.location
+        details.appendChild(city)
+        info.appendChild(details)
+        const open = document.createElement('button')
+        open.classList.add('open')
+        open.onclick = () => {
+            window.location.href = `http://localhost:3000/console/listings/${listing.id}`
+        }
+        const apply = document.createElement('div')
+        apply.classList.add('apply')
+        const btntext = document.createElement('span')
+        btntext.href = `console/listing/${listing.id}`
+        btntext.textContent = 'Review'
         apply.appendChild(btntext)
         open.appendChild(apply)
         info.appendChild(open)
